036efa33a881de7c61095b53b7cd56971f91c0d9
Jiri Crispeyn <jiri.crispeyn@gmail.com>
2018-08-23 01:31:15 +0200

    Setup rough layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,23 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import Header from "./components/header";
+import MyEditor from "./components/my-editor";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Header />
+        <div className="App-editor">
+          <div className="App-editor-left">
+            <div className="App-editor-title">Source</div>
+            <MyEditor />
+          </div>
+          <div className="App-editor-right">
+            <div className="App-editor-title">Destination</div>
+            <MyEditor className="App-editor-inner" />
+          </div>
+        </div>
       </div>
     );
   }
